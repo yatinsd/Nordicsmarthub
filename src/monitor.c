fb444cd63bd668cb42bb5dd191bec5d1fc7c42dc
yatinsd <37011467+yatinsd@users.noreply.github.com>
2023-11-12 13:30:14 +0530

    Update file structure

---
+++ src/monitor.c
@@ -0,0 +1,26 @@
+/*This file monitors all the states of the system including but not limited to
+1) Wifi connection
+2) Microcontroller states
+3) Input/Output states from the system
+4) Process optimization
+
+Author: Yatin
+*/ 
+
+float Monitor_activeswitch
+{
+
+
+}
+
+float Monitor_
+
+
+
+
+
+
+
+
+
+
